2b48a55eedd4addd28db378be0d13a46dffaf343
Elijah Nicasio <ElijahN8@gmail.com>
2019-05-25 03:35:44 -0700

    Player can now move left and right

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    const float speed = 6.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        this.transform.Translate(translation, 0f, 0f);
+    }
+}
