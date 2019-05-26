11bc349a0cbe23c69bc70bd4104e75e31c4bb480
Elijah Nicasio <ElijahN8@gmail.com>
2019-05-25 22:23:27 -0700

    Created GoodShape object that earns players points. Shapes spawn at intervals, and fall from the top of the screen.

--- Assets/Scripts/ShapeSpawner.cs
+++ Assets/Scripts/ShapeSpawner.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class ShapeSpawner : MonoBehaviour {
 
+    public GameObject badShape;
+    public GameObject goodShape;
+
+    private GameObject[] shapes;
+
+    private float t;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        shapes = new GameObject[] { badShape, goodShape };
+
+        t = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (t >= 2.0f)
+        {
+            float spawnPoint = Random.Range(Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, 0.0f)).x + 1.0f,
+                Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, 0.0f)).x - 1.0f);
+
+            int shapeInd = Random.Range(0, 2);
+
+            Instantiate(shapes[shapeInd], new Vector3(spawnPoint, this.transform.position.y, 0.0f), Quaternion.identity);
+
+            t = 0.0f;
+        }
+        else
+        {
+            t += Time.deltaTime;
+        }
 	}
 }
