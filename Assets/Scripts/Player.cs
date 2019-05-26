f945e6e96974ab28e8586aa467f6bd8a45f03ed0
Elijah Nicasio <ElijahN8@gmail.com>
2019-05-25 22:50:17 -0700

    Shapes disappear and interact with player correctly and on collision.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public int lives;
+    public int score;
+
     const float speed = 6.0f;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        lives = 3;
+        score = 0;
 	}
 	
 	// Update is called once per frame
@@ -17,5 +22,20 @@ public class Player : MonoBehaviour {
         float translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
         this.transform.Translate(translation, 0f, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.name.Contains("Good"))
+        {
+            score += 10;
+        }
+        if(collider.gameObject.name.Contains("Bad"))
+        {
+            if (lives > 0)
+                lives--;
+        }
+
+        Destroy(collider.gameObject);
     }
 }
